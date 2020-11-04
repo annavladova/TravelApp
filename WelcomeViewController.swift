073d786927686076042adf824c2877ba4b8e828d
Anna Vladova <73884389+annavladova@users.noreply.github.com>
2020-11-04 14:04:56 +0300

    Update WelcomeViewController.swift

--- WelcomeViewController.swift
+++ WelcomeViewController.swift
@@ -12,25 +12,15 @@ class WelcomeViewController: UIViewController {
     
     
 // MARK: - LIFECICLE
-//    !!!!!!!!! системная функция вьюдидлоад/ ее надо вызвать у вью контроллера (у родителя) только через супер/вьюдидлоад
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad WelcomeViewController")
         roundCorners(button: loginButton , radius: 4)
         roundCorners(button: createAccountButton , radius: 4)
         navigationItem.hidesBackButton = true
-//        let remoteConfig = RemoteConfig.remoteConfig()
-//        let loginText = remoteConfig["loginButtonText"].stringValue
-//        loginButton.setTitle(loginText, for: .normal)
-//        
-//        let isNeedToShowLoginButton = remoteConfig["isNeedToShowLoginButton"].boolValue
-//        if isNeedToShowLoginButton == false {
-//            loginButton.isHidden = true
-//        }
     }
-    
-//    вьюдидлоад срабатывает когда экран был загружен/ верстки еще нет. это значит что во вьюдидлоаде размеры всего такие какие они у нас установлены в сториборде (например под айфон 8)
-    
+
 // MARK: - ACTIONS
     
     @IBAction func loginClicked(_ sender: Any) {
@@ -45,8 +35,7 @@ class WelcomeViewController: UIViewController {
         let registrationVC = storybord.instantiateViewController(identifier: "RegistrationViewController") as! RegistrationViewController
         navigationController?.pushViewController(registrationVC, animated: true)
     }
-    
-    
+   
 // MARK: - FUNCTIONS
     
     func roundCorners(button: UIButton, radius: CGFloat) {
